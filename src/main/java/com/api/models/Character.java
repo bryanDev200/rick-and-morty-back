a385520b9eb605bcdade16e945dffcede7a0395d
bryan <brayanby2002@gmail.com>
2025-01-14 19:58:09 -0500

    Initial project's structure and empty components

---
+++ src/main/java/com/api/models/Character.java
@@ -0,0 +1,10 @@
+package com.api.models;
+
+public class Character {
+    private int id;
+    private String name;
+    private String status;
+    private String specie;
+    private String location;
+    private String image;
+}
